6a6cb1bffd97a103a65e74ce2a94df2925189402
Seyed <seyed.hashemi@swisscom.com>
2025-09-17 10:53:26 +0200

    init

--- Aufgabe-4/eslint.config.js
+++ Aufgabe-4/eslint.config.js
@@ -11,25 +11,38 @@ export default defineConfig([
     name: 'app/files-to-lint',
     files: ['**/*.{js,mjs,jsx,vue}'],
   },
-
   globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
-
   {
+    files: ['src/**/*', '**/*.vue'],
     languageOptions: {
       globals: {
         ...globals.browser,
       },
     },
   },
-
+  {
+    files: [
+      'scripts/**/*.{js,mjs}',
+      'eslint.config.{js,cjs,mjs}',
+      'vite.config.*',
+      'vitest.config.*',
+      'playwright.config.*',
+    ],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      'no-empty': ['error', { 'allowEmptyCatch': true }],
+    },
+  },
   js.configs.recommended,
   ...pluginVue.configs['flat/essential'],
-  
   {
     ...pluginVitest.configs.recommended,
     files: ['src/**/__tests__/*'],
   },
-  
   {
     ...pluginPlaywright.configs['flat/recommended'],
     files: ['e2e/**/*.{test,spec}.{js,ts,jsx,tsx}'],
